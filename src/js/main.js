dbc25c964e1e3e9d4b0062f6dda56e60a0e36f80
max-konokhov <makc.kohoxob@gmail.com>
2022-06-29 21:31:30 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// import './vendor/_gsap.min';
+// import './vendor/_inputmask.min';
+// import './vendor/_just-validate.min';
+// import './_search';
+// import './_burger';
+// import './_map';
+// import './_map-popup';
+// import './_valid-form';
